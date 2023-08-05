c1985406d588e0d144de787d260dfe266bf076d1
Alex_Nout <calium_by@mail.ru>
2023-08-05 17:18:29 +0300

    feat: create humburger menu

--- library/script.js
+++ library/script.js
@@ -18,3 +18,26 @@ console.log(`
         
         Общие требования к верстке +20
         `)
+
+const humburgerBtn = document.querySelector('.humburger_btn'),
+      nav = document.querySelector('nav ul');
+      header = document.querySelector('header');
+      humburgerLines = document.querySelectorAll('.humburger_line');
+humburgerBtn.addEventListener('click', (event)=>{
+    if (humburgerBtn.classList.contains('touched') && event.target !=nav) {
+        nav.style.display = 'none';
+        humburgerBtn.classList.remove('touched');
+        humburgerLines[0].style.display = 'block';
+        humburgerLines[1].style.transform =''
+        humburgerLines[2].style.transform =''
+
+    } else {
+        nav.style.display = 'flex';
+        humburgerBtn.classList.add('touched');
+        humburgerLines[0].style.display = 'none';
+        humburgerLines[1].style.transform ='rotate(45deg)'
+        humburgerLines[2].style.transform ='rotate(-45deg) translateX(5px) translateY(-5px)'
+
+        }
+  console.log(event.target)
+})
