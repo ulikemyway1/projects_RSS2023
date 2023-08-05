a6e02d6bff0e14da26f1f2d015599dfa8e2b33e8
Alex_Nout <calium_by@mail.ru>
2023-08-05 20:35:56 +0300

    refactor: burger menu pixel perfect fix

--- library/script.js
+++ library/script.js
@@ -1,43 +1,19 @@
-console.log(`
+window.addEventListener('DOMContentLoaded', ()=> {
+    const humburgerBtn = document.querySelector('.humburger_btn'),
+    nav = document.querySelector('nav ul'),
+    humburgerBtnLines = document.querySelectorAll('.humburger_line');
 
-Всего баллов: 100
+humburgerBtn.addEventListener('click', ()=>{
+  humburgerBtn.classList.toggle('touched');
+  nav.classList.toggle('open_nav');
+});
 
-    Вёрстка валидная +10
-
-    Вёрстка семантическая +16
-
-    Вёрстка соответствует макету +54
-        блок <header> +8:
-        секция Welcome +4.
-        секция About +6:
-        секция Favorites +8:
-        секция CoffeShop +6.
-        секция Contacts +6:
-        секция LibraryCard +8:
-        блок <footer> +8:
-        
-        Общие требования к верстке +20
-        `)
-
-const humburgerBtn = document.querySelector('.humburger_btn'),
-      nav = document.querySelector('nav ul');
-      header = document.querySelector('header');
-      humburgerLines = document.querySelectorAll('.humburger_line');
-humburgerBtn.addEventListener('click', (event)=>{
-    if (humburgerBtn.classList.contains('touched') && event.target !=nav) {
-        nav.style.display = 'none';
+document.addEventListener('click', (event)=>{
+    if(event.target != humburgerBtn && event.target !=nav && event.target != humburgerBtnLines[0] &&  event.target != humburgerBtnLines[1] &&  event.target != humburgerBtnLines[2] ) {
         humburgerBtn.classList.remove('touched');
-        humburgerLines[0].style.display = 'block';
-        humburgerLines[1].style.transform =''
-        humburgerLines[2].style.transform =''
+        nav.classList.remove('open_nav');
+    };
+});
+});
+ 
 
-    } else {
-        nav.style.display = 'flex';
-        humburgerBtn.classList.add('touched');
-        humburgerLines[0].style.display = 'none';
-        humburgerLines[1].style.transform ='rotate(45deg)'
-        humburgerLines[2].style.transform ='rotate(-45deg) translateX(5px) translateY(-5px)'
-
-        }
-  console.log(event.target)
-})
