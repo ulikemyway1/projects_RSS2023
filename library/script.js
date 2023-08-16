adc35d328d2cee2b1146acc1be95f4715c683176
Alex_Nout <calium_by@mail.ru>
2023-08-16 14:10:35 +0300

    feat: ABOUT SLIDER -  implemented switching with buttons

--- library/script.js
+++ library/script.js
@@ -10,7 +10,6 @@ humburgerBtn.addEventListener('click', ()=>{
 });
 
 document.addEventListener('click', (event)=>{
-    console.log(event.target)
     if(event.target != humburgerBtn && event.target !=nav && event.target != humburgerBtnLines[0] &&  event.target != humburgerBtnLines[1] &&  event.target != humburgerBtnLines[2] ) {
         humburgerBtn.classList.remove('touched');
         nav.classList.remove('open_nav');
@@ -19,6 +18,33 @@ document.addEventListener('click', (event)=>{
 
 
 //About SLIDER
+const sliderContent = document.querySelector('.about_slider_content');
+const sliderBtns = document.querySelectorAll('.about_slider_paginator-btn-fill');
+const sliderBtnsWithPadding = document.querySelectorAll('.about_slider_paginator-btn');
+const sliderBtnsPanel = document.querySelector('.about_slider_paginator_wrapper');
+const sliderArrowLeft = document.querySelector('.about_slider_arrow-left');
+const sliderArrowRigth = document.querySelector('.about_slider_arrow-rigth');
+const sliderShift = 475;
+
+sliderBtnsWithPadding.forEach((item, index)=>{
+    item.addEventListener('click', (e)=>{
+        if (!item.classList.contains('paginator-btn_active')) {
+           sliderContent.style.left = `-${sliderShift*index}px`;
+           sliderBtns.forEach((btn)=>{
+           btn.classList.remove('paginator-btn_active');
+           sliderBtns[index].classList.add('paginator-btn_active');
+
+        })  
+        }
+       
+    })
+})
+// sliderBtnsPanel.addEventListener('click', (event)=>{
+//     console.log(event.target)
+//     if (event.target == sliderBtns[0]) {
+//         sliderContent.style.left = '-475px';
+//     }
+// })
 
 });
 
