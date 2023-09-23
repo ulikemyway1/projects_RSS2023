3fb868fbef13d68a1cdcacdc3445550cb870ff3b
Alex_Nout <calium_by@mail.ru>
2023-09-23 17:16:47 +0300

    feat: add basic search

---
+++ image-galery/js/index.js
@@ -0,0 +1,20 @@
+let url = '';
+function getPhotos() {
+    fetch(url)
+      .then((obj) => obj.json())
+      .then((data) => {
+        // console.log(data);
+        showPhotos(data)
+      });
+      
+  }
+//   getData();
+
+document.querySelector('input').addEventListener('blur', (event)=> {
+url = `https://api.unsplash.com/search/photos?query=${document.querySelector('input').value}&per_page=0&orientation=landscape&client_id=4colJrPVgUKsfB2OkwF3G9KmiHb72P493LNMEQ1MVJ0`
+getPhotos();
+});
+
+function showPhotos(data) {
+    document.querySelector('div').style.background = `url('${data.results[0].urls.full}') center center/cover`;
+}
