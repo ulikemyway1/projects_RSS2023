e8ccb8b55aabff62f2d381512c72674828fc48a4
Alex_Nout <calium_by@mail.ru>
2023-09-24 12:59:46 +0300

    feat: add grid layout and set-up search properties

--- image-galery/js/index.js
+++ image-galery/js/index.js
@@ -3,17 +3,29 @@ function getPhotos() {
     fetch(url)
       .then((obj) => obj.json())
       .then((data) => {
-        // console.log(data);
+        console.log(data);
         showPhotos(data)
       });
       
   }
 
-document.querySelector('input').addEventListener('blur', (event)=> {
-url = `https://api.unsplash.com/search/photos?query=${document.querySelector('input').value}&per_page=0&orientation=landscape&client_id=4colJrPVgUKsfB2OkwF3G9KmiHb72P493LNMEQ1MVJ0`
+const searchField = document.querySelector('input');
+searchField.addEventListener('change', (event)=> {
+url = `https://api.unsplash.com/search/photos?query=${document.querySelector('input').value}&per_page=6&orientation=landscape&order_by=relevant&client_id=4colJrPVgUKsfB2OkwF3G9KmiHb72P493LNMEQ1MVJ0`
 getPhotos();
 });
 
 function showPhotos(data) {
-    document.querySelector('div').style.background = `url('${data.results[0].urls.full}') center center/cover`;
+    const contents = document.querySelectorAll('.content');
+    data.results.forEach((item, index) => {
+      contents[index].style.background = `url('${item.urls.regular}') center center/cover`;
+    })
 }
+
+const searchBtn = document.querySelector('.search button');
+
+searchField.addEventListener('input', () => {
+  if (searchField.value.length !== 0) {
+    searchBtn.textContent = 'X';
+  } else  searchBtn.textContent = 'O';
+})
