f45b80cad655f28c36e2af1b39485d0d32a207f4
ulikemyway <ulikemyway>
2023-10-07 14:09:19 +0300

    feat: sumbit winner form by enter

--- random-game/js/script.js
+++ random-game/js/script.js
@@ -272,7 +272,7 @@ function countScore() {
         win()
     }
 }
-
+const winForm = document.getElementById('win_form');
 function win() {
     clearInterval(timerUp);
     clearInterval(timerDown);
@@ -289,7 +289,11 @@ function win() {
         sendNameBtn.disabled = true;
     }
     })
-sendNameBtn.addEventListener('click', () => {
+sendNameBtn.addEventListener('click', makeRecord);
+winForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+function makeRecord () {
     if (playerName.value.trim().length > 0) {
         saveLocal(playerName.value.trim());
         playerName.value = '';
@@ -297,9 +301,11 @@ sendNameBtn.addEventListener('click', () => {
             winPanel.classList.add('hidden')
         }, 400);
         location.reload();
-    } 
-})
+    }
 }
+}
+
+
 
 function saveLocal(name) {
     let player = new Object;
